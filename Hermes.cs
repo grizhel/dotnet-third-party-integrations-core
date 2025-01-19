a0ce39e5161191e1e48aee2fd7e655c1c5ff46a0
grizhel <emreyilmazin@gmail.com>
2025-01-19 21:32:04 +0300

    dotnet_utilities_core is used

--- Hermes.cs
+++ Hermes.cs
@@ -10,75 +10,74 @@ using System.Threading.Tasks;
 
 using Confluent.Kafka;
 using dotnet_third_party_integrations_core.kafka.models;
-using dotnet_third_party_integrations_core.utils;
+using dotnet_utilities_core.Base.JsonUtilities;
 
 
-namespace dotnet_third_party_integrations_core.Kafka
+namespace dotnet_third_party_integrations_core.Kafka;
+
+public static class Hermes
 {
-	public static class Hermes
+	public static async Task SendMessageAsync(KafkaOptions conf, string topic, object data)
 	{
-		public static async Task SendMessageAsync(KafkaOptions conf, string topic, object data)
+		using (var p = new ProducerBuilder<Null, string>(conf.GetConfig()).Build())
 		{
-			using (var p = new ProducerBuilder<Null, string>(conf.GetConfig()).Build())
+			await p.ProduceAsync(topic, new Message<Null, string>
 			{
-				await p.ProduceAsync(topic, new Message<Null, string>
-				{
-					Value = JsonSerializer.Serialize(data, TheThird.JsonSerializer.GetJsonSerializerOptions())
-				});
-			}
+				Value = JsonSerializer.Serialize(data, JsonSerializerUtility.GetJsonSerializerOptions())
+			});
 		}
+	}
 
-		public static async Task SubscribeAsync(KafkaOptions conf, string topic, Func<string, Task> act)
+	public static async Task SubscribeAsync(KafkaOptions conf, string topic, Func<string, Task> act)
+	{
+		using (var c = new ConsumerBuilder<Ignore, string>(conf.GetConfig()).Build())
 		{
-			using (var c = new ConsumerBuilder<Ignore, string>(conf.GetConfig()).Build())
+			c.Subscribe(topic);
+
+			CancellationTokenSource cts = new CancellationTokenSource();
+			Console.CancelKeyPress += (_, e) =>
 			{
-				c.Subscribe(topic);
+				e.Cancel = true;
+				cts.Cancel();
+			};
 
-				CancellationTokenSource cts = new CancellationTokenSource();
-				Console.CancelKeyPress += (_, e) =>
+			try
+			{
+				while (!cts.IsCancellationRequested)
 				{
-					e.Cancel = true;
-					cts.Cancel();
-				};
-
-				try
-				{
-					while (!cts.IsCancellationRequested)
+					try
 					{
-						try
-						{
-							var cr = c.Consume();
-							await act(cr.Message.Value);
-						}
-						catch (ConsumeException e)
-						{
-							Console.WriteLine($"Error occured: {e.Error.Reason}");
-						}
-						catch (Exception e)
-						{
-							Console.WriteLine($"Error occured: {e.Message}");
-						}
-						finally
-						{
-							Thread.Sleep(2000);
-						}
+						var cr = c.Consume();
+						await act(cr.Message.Value);
+					}
+					catch (ConsumeException e)
+					{
+						Console.WriteLine($"Error occured: {e.Error.Reason}");
+					}
+					catch (Exception e)
+					{
+						Console.WriteLine($"Error occured: {e.Message}");
+					}
+					finally
+					{
+						Thread.Sleep(2000);
 					}
 				}
-				catch (OperationCanceledException e)
-				{
-					Console.WriteLine(e.Message);
-				}
-				finally
-				{
-					c.Unsubscribe();
-					c.Close();
-				}
+			}
+			catch (OperationCanceledException e)
+			{
+				Console.WriteLine(e.Message);
+			}
+			finally
+			{
+				c.Unsubscribe();
+				c.Close();
 			}
 		}
+	}
 
-		public static bool IsRunning(KafkaOptions conf)
-		{
-			return conf.IsRunning();
-		}
+	public static bool IsRunning(KafkaOptions conf)
+	{
+		return conf.IsRunning();
 	}
 }
